8f596d1db73f2be72eb735abc4d9c82067e5194c
Brandon Lilly <brandonjlilly@gmail.com>
2015-03-31 17:29:41 -0700

    Use routes and templates

--- app/assets/javascripts/pokedex-2.js
+++ app/assets/javascripts/pokedex-2.js
@@ -1,47 +1,57 @@
 Pokedex.RootView.prototype.addToyToList = function (toy) {
-  var $li = $('<li class="toy-list-item">');
-  $li.data('id', toy.get('id'));
-  $li.data('pokemon-id', toy.get('pokemon_id'));
+  // var $li = $('<li class="toy-list-item">');
+  // $li.data('id', toy.get('id'));
+  // $li.data('pokemon-id', toy.get('pokemon_id'));
+  //
+  // var shortInfo = ['name', 'happiness', 'price'];
+  // shortInfo.forEach(function (attr) {
+  //   $li.append(attr + ': ' + toy.get(attr) + '<br>');
+  // });
+  //
+  // this.$pokeDetail.find(".toys").append($li);
 
-  var shortInfo = ['name', 'happiness', 'price'];
-  shortInfo.forEach(function (attr) {
-    $li.append(attr + ': ' + toy.get(attr) + '<br>');
-  });
-
-  this.$pokeDetail.find(".toys").append($li);
+  // debugger
+  var template = JST["toyListItem"];
+  var content = template({ toy: toy });
+  this.$pokeDetail.find(".toys").append(content);
 };
 
 Pokedex.RootView.prototype.renderToyDetail = function (toy) { // III
-  this.$toyDetail.empty();
+  // this.$toyDetail.empty();
+  //
+  // var $detail = $('<div class="detail">');
+  // $detail.append('<img src="' + toy.get('image_url') + '"><br>');
+  // for (var attr in toy.attributes) {
+  //   if(attr !== 'pokemon_id' && attr !== 'image_url') {
+  //     var $span = $('<span style="font-weight:bold;">');
+  //     $span.html(attr + ': ');
+  //     $detail.append($span);
+  //     $detail.append(toy.get(attr));
+  //     $detail.append('<br>');
+  //   }
+  // }
+  //
+  // // Phase III
+  // var $pokemonSelect = $('<select>');
+  // $pokemonSelect.attr("data-pokemon-id", toy.get("pokemon_id"));
+  // $pokemonSelect.attr("data-toy-id", toy.id);
+  // this.pokes.each(function (pokemon) {
+  //   var $pokemonOption = $('<option>');
+  //   $pokemonOption.attr("value", pokemon.id);
+  //   $pokemonOption.text(pokemon.get("name"));
+  //   if (pokemon.id == toy.get("pokemon_id")) {
+  //     $pokemonOption.prop("selected", true);
+  //   }
+  //   $pokemonSelect.append($pokemonOption);
+  // });
+  // $detail.append($pokemonSelect);
+  //
+  // this.$toyDetail.html($detail);
 
-  var $detail = $('<div class="detail">');
-  $detail.append('<img src="' + toy.get('image_url') + '"><br>');
-  for (var attr in toy.attributes) {
-    if(attr !== 'pokemon_id' && attr !== 'image_url') {
-      var $span = $('<span style="font-weight:bold;">');
-      $span.html(attr + ': ');
-      $detail.append($span);
-      $detail.append(toy.get(attr));
-      $detail.append('<br>');
-    }
-  }
 
-  // Phase III
-  var $pokemonSelect = $('<select>');
-  $pokemonSelect.data("pokemon-id", toy.get("pokemon_id"));
-  $pokemonSelect.data("toy-id", toy.id);
-  this.pokes.each(function (pokemon) {
-    var $pokemonOption = $('<option>');
-    $pokemonOption.attr("value", pokemon.id);
-    $pokemonOption.text(pokemon.get("name"));
-    if (pokemon.id == toy.get("pokemon_id")) {
-      $pokemonOption.prop("selected", true);
-    }
-    $pokemonSelect.append($pokemonOption);
-  });
-  $detail.append($pokemonSelect);
-
-  this.$toyDetail.html($detail);
+  var template = JST['toyDetail'];
+  var content = template({ toy: toy, pokes: this.pokes });
+  this.$toyDetail.html(content);
 };
 
 Pokedex.RootView.prototype.selectToyFromList = function (event) {
--- app/assets/javascripts/pokedex-5.js
+++ app/assets/javascripts/pokedex-5.js
@@ -2,47 +2,108 @@ Pokedex.Views = {}
 
 Pokedex.Views.PokemonIndex = Backbone.View.extend({
   events: {
+    'click li':'selectPokemonFromList'
   },
 
   initialize: function () {
+    this.collection = new Pokedex.Collections.Pokemon();
   },
 
   addPokemonToList: function (pokemon) {
+    var template = JST['pokemonListItem'];
+    var content = template({ pokemon: pokemon });
+    this.$el.append(content);
   },
 
   refreshPokemon: function (options) {
+    this.collection.fetch( {
+      success: function () {
+        this.render();
+        if (options) { options.success(); }
+      }.bind(this)
+    });
   },
 
   render: function () {
+    this.$el.empty();
+    this.collection.each( function (pokemon) {
+      this.addPokemonToList(pokemon);
+    }.bind(this));
   },
 
   selectPokemonFromList: function (event) {
+    var id = $(event.currentTarget).data('id');
+    Backbone.history.navigate('pokemon/' + id, { trigger: true })
   }
 });
 
 Pokedex.Views.PokemonDetail = Backbone.View.extend({
   events: {
+    'click ul.toys li':'selectToyFromList'
   },
 
   refreshPokemon: function (options) {
+    this.model.fetch({
+      success: function (response) {
+        this.render();
+        if (options) { options.success(); }
+      }.bind(this)
+    })
   },
 
   render: function () {
+    // debugger
+    var content = JST['pokemonDetail']({ pokemon: this.model });
+    this.$el.html(content);
+
+    this.model.toys().each(function (toy) {
+      var content = JST['toyListItem']({ toy: toy });
+      this.$el.find('ul.toys').append(content);
+    }.bind(this))
+    return this;
   },
 
   selectToyFromList: function (event) {
+    var toyId = $(event.currentTarget).data('id');
+    var pokemonId = $(event.currentTarget).data('pokemon-id');
+    Backbone.history.navigate(
+      'pokemon/' + pokemonId + '/toys/' + toyId,
+      { trigger: true }
+    );
   }
 });
 
 Pokedex.Views.ToyDetail = Backbone.View.extend({
+  events: {
+    'change select':'reassignToy'
+  },
+
+  initialize: function (options) {
+    this.pokes = options.pokes;
+  },
+
   render: function () {
+    // debugger
+    var content = JST['toyDetail']({ pokes: this.pokes, toy: this.model });
+    this.$el.html(content);
+    return this;
+  },
+
+  reassignToy: function (event) {
+    var $select = $(event.currentTarget);
+    var pokemon = this.pokes.get($select.data("pokemon-id"));
+    var toy = pokemon.toys().get($select.data("toy-id"));
+
+    toy.set("pokemon_id", $select.val());
+    toy.save({}, {
+      success: (function () {
+        pokemon.toys().remove(toy);
+        // this.$el.empty();
+        Backbone.history.navigate(
+          'pokemon/' + toy.get('pokemon_id') + '/toys/' + toy.id,
+          { trigger: true }
+        );
+      }).bind(this)
+    });
   }
 });
-
-/*
-$(function () {
-  var pokemonIndex = new Pokedex.Views.PokemonIndex();
-  pokemonIndex.refreshPokemon();
-  $("#pokedex .pokemon-list").html(pokemonIndex.$el);
-});
-*/
